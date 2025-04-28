31ce7f843ea1fcdeb7a7bf63d54430d0885e13e0
Guilho-19 <guilhermepalaronunessilva@gmail.com>
2025-04-28 16:22:03 -0300

    Exercício 01

---
+++ src/exercicios/java/basico/exercicio01/ContaBancaria.java
@@ -0,0 +1,168 @@
+package exercicios.java.basico.exercicio01;
+
+import java.util.Scanner;
+
+public class ContaBancaria {
+	private Scanner scanner = new Scanner(System.in);
+	private double chequeEspecial;
+	private double valorUsarChequeEspecial = 0;
+	private double saldo = 0;
+	
+	public static void main(String[] args) {
+		ContaBancaria conta = new ContaBancaria();
+		conta.menu();
+	}
+	
+	private void menu() {
+		int menuEscolhido;
+		
+		do {
+			System.out.println("=== BANCO DIGITAL ===");
+			System.out.println("\nMENU PRINCIPAL");
+			System.out.println("1 - Consultar saldo");
+			System.out.println("2 - Realizar depósito");
+			System.out.println("3 - Realizar saque");
+			System.out.println("4 - Pagar boleto");
+			System.out.println("5 - Realizar primeiro depósito");
+			System.out.println("0 - Sair");
+			
+			while (!scanner.hasNextInt()) {
+				System.out.println("Por favor, digite um número válido!");
+				scanner.next();
+			}
+			
+			menuEscolhido = scanner.nextInt();
+		
+			switch (menuEscolhido) {
+			case 1 -> consultarSaldo();
+			case 2 -> deposito();
+			case 3 -> saque();
+			case 4 -> pagarBoleto();
+			case 5 -> calculoChequeEspecial();
+			case 0 -> System.out.println("Obrigado por usar nossos serviços!");
+			default -> System.out.println("Opção inválida");
+			}
+		} while (menuEscolhido != 0);
+	}
+	
+	private void consultarSaldo() {
+		System.out.println("O valor do saldo atual da sua conta é de R$" + saldo);
+		if (valorUsarChequeEspecial > 0) {
+			System.out.println("Você está utilizando cheque especial no valor de R$" + valorUsarChequeEspecial);
+		}
+	}
+	
+	private void calculoChequeEspecial() {
+		if (saldo == 0) {
+			System.out.println("Qual será o valor do seu depósito inicial?");
+			double depositoInicial = scanner.nextDouble();
+			
+			if (depositoInicial <= 500) {
+				saldo += depositoInicial;
+				chequeEspecial = 50;
+				System.out.println("Depósito no valor de R$" + depositoInicial + " feito com sucesso");
+				System.out.println("Seu cheque especial é de R$" + chequeEspecial);
+			} else if (depositoInicial > 500) {
+				chequeEspecial = depositoInicial * 0.50;
+				saldo += depositoInicial;
+				System.out.println("Depósito no valor de R$" + depositoInicial + " feito com sucesso");
+				System.out.println("Seu cheque especial é de R$" + chequeEspecial);
+			}
+		} else {
+			System.out.println("Você já realizou seu primeiro depósito");
+		}
+	}
+	
+	private void deposito() {
+		System.out.println("Qual será o valor do seu depósito?");
+		double valorDeposito = scanner.nextDouble();
+		
+		if (valorDeposito <= 0) {
+			System.out.println("Valor de depósito inválido");
+		} else {
+			if (valorUsarChequeEspecial > 0) {
+				if (valorDeposito >= valorUsarChequeEspecial) {
+					double restanteDeposito = valorDeposito - valorUsarChequeEspecial;
+	                System.out.println("R$" + valorUsarChequeEspecial + " do seu depósito foi usado para quitar o cheque especial");
+	
+	                chequeEspecial += valorUsarChequeEspecial;
+	                valorUsarChequeEspecial = 0;
+	
+	                saldo += restanteDeposito;
+	                System.out.println("Depósito no valor de R$" + valorDeposito + " processado com sucesso");
+	                System.out.println("R$" + restanteDeposito + " foi adicionado ao seu saldo");
+				} else {
+					valorUsarChequeEspecial -= valorDeposito;
+					chequeEspecial += valorDeposito;
+					
+					System.out.println("Seu depósito de R$" + valorDeposito + " foi integralmente utilizado para reduzir sua dívida de cheque especial");
+					System.out.println("Você ainda está utilizando R$" + valorUsarChequeEspecial + " do cheque especial");
+				}
+			} else {
+				saldo += valorDeposito;
+				System.out.println("Depósito no valor de R$" + valorDeposito + " efetuado com sucesso");
+				System.out.println("Seu novo saldo é de R$" + saldo);
+			}
+		}
+	}
+	
+	private void saque() {
+		System.out.println("Qual será o valor do seu saque?");
+		double valorSaque = scanner.nextDouble();
+		
+		if (valorSaque <= 0) {
+			System.out.println("Valor de saque inválido");
+		} else {
+			if (valorSaque <= saldo) {
+				saldo -= valorSaque;
+				System.out.println("Saque no valor de R$" + valorSaque + " efetuado com sucesso");
+				System.out.println("Seu novo saldo é de R$" + saldo);
+			} else if (valorSaque <= (saldo + chequeEspecial)){
+				valorUsarChequeEspecial = valorSaque - saldo;
+				double taxa = valorUsarChequeEspecial * 0.2;
+				double totalDebitado = valorUsarChequeEspecial + taxa;
+				
+				saldo = 0;
+				chequeEspecial -= totalDebitado;
+				valorUsarChequeEspecial += totalDebitado;
+				
+				System.out.println("Saque no valor de R$" + valorSaque + " efetuado com sucesso");
+				System.out.println("Seu novo saldo é de R$" + saldo);
+				System.out.println("Você entrou no cheque especial");
+			} else {
+				System.out.println("Saldo insuficiente para concluir a transação");
+				System.out.println("Seu saldo (incluindo cheque especial): R$" + (saldo + chequeEspecial));
+			}
+		}
+	}
+	
+	private void pagarBoleto() {
+		System.out.println("Qual será o valor do boleto que você irá pagar?");
+		double valorBoleto = scanner.nextDouble();
+		
+		if (valorBoleto <= 0) {
+			System.out.println("Valor do boleto inválido");
+		} else {
+			if (valorBoleto <= saldo) {
+				saldo = saldo - valorBoleto;
+				System.out.println("Pagamento do boleto no valor de R$" + valorBoleto + " efetuado com sucesso");
+				System.out.println("Seu novo saldo é de R$" + saldo);
+			} else if (valorBoleto <= (saldo + chequeEspecial)){
+				valorUsarChequeEspecial = valorBoleto - saldo;
+				double taxa = valorUsarChequeEspecial * 0.2;
+				double totalDebitado = valorUsarChequeEspecial + taxa;
+				
+				saldo = 0;
+				chequeEspecial -= totalDebitado;
+				valorUsarChequeEspecial += totalDebitado;
+				
+				System.out.println("Boleto no valor de R$" + valorBoleto + " pago com sucesso");
+				System.out.println("Seu novo saldo é de R$" + saldo);
+				System.out.println("Você entrou no cheque especial");
+			} else {
+				System.out.println("Saldo insuficiente para concluir a transação");
+				System.out.println("Seu saldo (incluindo cheque especial): R$" + (saldo + chequeEspecial));
+			}
+		}	
+	}
+}
